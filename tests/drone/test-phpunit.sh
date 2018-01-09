3b0f9e577f54c6880155b60999a345e207cf83ac
SergioBertolinSG <sbertolin@solidgear.es>
2018-01-09 12:00:22 +0100

    Running Specific external storage tests

--- tests/drone/test-phpunit.sh
+++ tests/drone/test-phpunit.sh
@@ -65,7 +65,7 @@ fi
 set_up_external_storage() {
     ./occ app:enable files_external
     case "${FILES_EXTERNAL_TYPE}" in
-    webdav)
+    Owncloud)
         wait-for-it owncloud_external:80
         cat > config/config.webdav.php <<DELIM
  <?php
@@ -86,18 +86,21 @@ DELIM
     esac
 }
 
+FILES_EXTERNAL_BACKEND_PATH=../apps/files_external/tests/Storage
+FILES_EXTERNAL_TEST_TO_RUN=${FILES_EXTERNAL_TYPE}Test.php
+
 
 if [[ ${ENABLE_COVERAGE} == "true" ]]; then
     if [[ -n ${FILES_EXTERNAL_TYPE} ]]; then
         set_up_external_storage
-        exec phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-${DB_TYPE}.xml
+        exec phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-${DB_TYPE}.xml ${FILES_EXTERNAL_BACKEND_PATH}/${FILES_EXTERNAL_TEST_TO_RUN}
     else
         exec phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --coverage-clover tests/autotest-clover-${DB_TYPE}.xml
     fi
 else
     if [[ -n ${FILES_EXTERNAL_TYPE} ]]; then
         set_up_external_storage
-        exec ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --log-junit tests/autotest-external-results-${DB_TYPE}.xml
+        exec ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --log-junit tests/autotest-external-results-${DB_TYPE}.xml ${FILES_EXTERNAL_BACKEND_PATH}/${FILES_EXTERNAL_TEST_TO_RUN}
     else
         exec ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --log-junit tests/autotest-results-${DB_TYPE}.xml    
     fi
